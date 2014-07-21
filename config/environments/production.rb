be2902df778f8392098a4f866e4dce454cae289b
Karolis Astrauka <astrauka@gmail.com>
2014-07-21 23:25:39 +0300

    update production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Expensable::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { host: 'expensable.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'expensable4.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:        'smtp.sendgrid.net',
